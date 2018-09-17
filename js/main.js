f21c50b37cb56be87cb7e1916de9358c59462b55
Marissa <marissaschmucker@gmail.com>
2018-09-17 12:02:38 -0500

    add click event

--- js/main.js
+++ js/main.js
@@ -5,3 +5,14 @@
 
 //cat image
 const cat = document.querySelector(".cat");
+
+//click score
+const score = document.querySelector(".score");
+
+//add event listener to listen for click event and increment score
+cat.addEventListener("click", function() {
+    //get current score
+    let clicks = score.innerHTML;
+    //increase current score by 1
+    score.innerHTML = parseInt(clicks) + 1;
+});
