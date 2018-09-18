706b181cbc0665e0120163223dd47e48d859b8bc
Marissa <marissaschmucker@gmail.com>
2018-09-18 12:10:16 -0500

    add closure to event listener

--- js/main.js
+++ js/main.js
@@ -10,27 +10,40 @@ const catGroup = document.querySelector(".cat-group");
 const score = document.querySelector(".score");
 
 //add each cat to catGroup
-for (cat in allCats) {
+for (cat of allCats) {
 
-    //create cat img element
+    //create cat img element and p element for name
     let catImg = document.createElement('img');
+    let nameTag = document.createElement('p');
 
-    //add class, id, source img,  and alt text
+    //add class, id, source img,  and alt text to img
     catImg.className = 'cat';
     catImg.id = cat.name;
-    catImg.src = 'img/cat.png';
+    catImg.src = cat.src;
     catImg.alt = 'cat image';
 
-    //add click event to cat
-    catImg.addEventListener('click', function() {
-        //get current score
-        let clicks = score.innerHTML;
-        //increase current score by 1
-        cat.clicked += 1  //update individual score
-        score.innerHTML = parseInt(clicks) + 1; //add to total score by updating DOM
-    }, false);
+    //add class to p tag
+    nameTag.className = 'name';
+    nameTag.innerHTML = cat.name;
 
-    //append img to catGroup
-    catGroup.appendChild(catImg);
+    //add click event to cat
+    catImg.addEventListener('click', (function(catCopy) {
+        return function() {
+            //get current score
+            let clicks = score.innerHTML;
+            //increase current score by 1
+            catCopy.clicked += 1  //update individual score
+            score.innerHTML = parseInt(clicks) + 1; //add to total score by updating DOM
+        };
+    })(cat));
+
+    const addElements = function () {
+        for (let i = 0; i < arguments.length; i++) {
+            //append element to catGroup
+            catGroup.appendChild(arguments[i]);
+        }
+    };
+
+    document.body.onload = addElements(catImg,nameTag);
 
 }
--- js/state.js
+++ js/state.js
@@ -1,6 +1,6 @@
 //this data object holds the state of each cat
 
 const allCats = [
-    { id: 1, name: "Henry", clicked: 0 },
-    { id: 2, name: "Fluffy", clicked: 0 },
+    { src: 'img/cat.png', name: "Henry", clicked: 0 },
+    { src: 'img/cat.png', name: "Fluffy", clicked: 0 },
 ];
