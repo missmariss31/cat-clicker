bc4b3ac433e192a22d029737137f9d0b2cb4bbee
Marissa <marissaschmucker@gmail.com>
2018-09-29 08:54:41 -0500

    add comments

--- js/main.js
+++ js/main.js
@@ -3,13 +3,16 @@
 */
 
 const update = {
+    
     showCat: function(cat) {
+        // change state of visible to true
         cat.visible = true;
+        // render the page with updated state
         view.render();
     },
 
     addToClick: function(catEl) {
-
+        // add clicks to clicked cat
         if (catEl.className === 'cat one') {
             allCats[0].clickCount += 1;
         }
@@ -19,13 +22,14 @@ const update = {
         if (catEl.className === 'cat three') {
             allCats[2].clickCount += 1;
         }
-
+        // add clicks to total
         totalClicks += 1;
-
+        // render the page with updated state
         view.render();
     },
 
     changeName: function(buttonEl, newName) {
+        // change the name of the kitten selected
         if (buttonEl.id === 'one') {
             allCats[0].name = newName;
         }
@@ -36,6 +40,7 @@ const update = {
             allCats[2].name = newName;
         }
 
+        // render the page with updated state
         view.render();
     }
 
